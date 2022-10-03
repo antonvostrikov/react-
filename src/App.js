8a0f69b57bd3578665543d1aa87ca1a472f0c4f5
antonvostrikov <42900827+antonvostrikov@users.noreply.github.com>
2022-10-03 22:55:18 +0300

    lists

--- src/App.js
+++ src/App.js
@@ -24,15 +24,17 @@ class App extends Component {
   }
 
   render() {
-    const cars = this.state.cars
+    // const cars = this.state.cars
     return (
       <div className="App" style={{width: '600px', margin: '0 auto'}}>
         <span>{this.state.title}</span>
         <input type="text" onChange={this.changeInput} />
         <button onClick={this.changeColor}>Add Title</button>
-        <Car car={cars[0].car} model={cars[0].model} engine={cars[0].engine} color={cars[0].color} changeTitle={this.changeTitle.bind(this, cars[0].car)}/> 
-        <Car car={cars[1].car} model={cars[1].model} engine={cars[1].engine} color={cars[1].color} changeTitle={() => this.changeTitle(cars[1].car)}/>
-        <Car car={cars[2].car} model={cars[2].model} engine={cars[2].engine} color={cars[2].color} changeTitle={this.changeTitle.bind(this, cars[2].car)}/>
+        { this.state.cars.map((car, index) => {
+          return (
+            <Car key={index} car={car.car} model={car.model} engine={car.engine} color={car.color} changeTitle={() => this.changeTitle(car.car)}/>
+          )
+        }) }
       </div>
     );
   }
